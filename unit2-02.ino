31fd04f16ca22bbc9e6de3b4024c1701f8ea63d8
Cristiano Sellitto <99354780+CristianoSellitto@users.noreply.github.com>
2023-02-28 09:55:36 -0500

    Create unit2-02.ino

---
+++ unit2-02.ino
@@ -0,0 +1,20 @@
+
+/*
+  Created by: Cristiano S
+  Created on: February 2023
+
+  Turns an LED on for one second, then off for one second, repeatedly.
+*/
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
